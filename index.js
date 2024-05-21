1a2f345c3526990b28d5b919b01d88172017cd88
melcholign <115886450+melcholign@users.noreply.github.com>
2024-05-22 01:30:19 +0600

    Initialize login and registration post endpoints

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const cors = require('cors')
 const path = require('path')
 
 const app = express()
 app.use(express.static(__dirname))
-app.use(cors)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'login.html'))
+    res.send('Hello world')
 })
 
 app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname,'login.html'))
 })
 
+app.post('/login', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+
 app.get('/registration', (req, res) => {
     res.sendFile(path.join(__dirname, 'registration.html'))
 })
 
-app.listen(port, () => console.log(`Server is started at http://localhost:${port}/...`))
+app.post('/registration', (req, res) => {
+    console.log(req.body)
+    res.json(req.body)
+})
+
+app.listen(port, () => console.log(`Server is started at http://localhost:${port}/`))
