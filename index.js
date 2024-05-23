24ff40cacef72830c70423e01a6f4a4386ff4824
melcholign <115886450+melcholign@users.noreply.github.com>
2024-05-23 19:59:40 +0600

    Implement the login post api

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser')
 const mysql = require('mysql')
 const cors = require('cors')
 const path = require('path')
-const { ok } = require('assert')
 
 // establish connection and connect with the database
 const connection = mysql.createConnection({
@@ -45,9 +44,17 @@ app.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, 'login.html'))
 })
 
-app.post('/login', (req, res) => {
-    console.log(req.body)
-    res.json(req.body)
+app.post('/login', async (req, res) => {
+    const data = req.body
+
+    const { results } = await executeQuery(`SELECT * FROM profile WHERE email = '${data.email}' AND password = '${data.password}'`)
+
+    if (results.length === 0) {
+        res.status(400).send('The email or password you have entered is incorrect.')
+        return
+    }
+
+    res.sendStatus(200)
 })
 
 // registration endpoints
@@ -60,12 +67,10 @@ app.post('/registration', async (req, res) => {
 
     const { results } = await executeQuery(`SELECT email from profile where email = '${data.email}'`)
 
-    console.log(results)
-
     if (results.length != 0) {
         res.status(400).send(`A profile with the email ${results[0].email} is already registered.`)
         return
-    } 
+    }
 
     await executeQuery(`INSERT INTO profile (first_name, last_name, gender, date_of_birth, email, password, image_url)
             VALUES ('${data.first_name}', '${data.last_name}', '${data.gender}', '${data.date_of_birth}', '${data.email}', '${data.password}', null)`)
@@ -85,9 +90,9 @@ function executeQuery(query) {
             if (err) {
                 reject(err)
             }
-            
+
             resolve({ results, fields })
-            
+
         })
     })
 }
---
+++ login.js
@@ -0,0 +1,81 @@
+const submitBtn = document.querySelector('#submit')
+const emailField = document.querySelector('#email')
+const passwordField = document.querySelector('#password')
+const submissionError = document.querySelector('.submission-error')
+
+emailField.addEventListener('input', () => removeFieldError(emailField))
+passwordField.addEventListener('input', () => removeFieldError(passwordField))
+
+submitBtn.addEventListener('click', e => {
+    e.preventDefault()
+
+    if (validateForm()) {
+
+        submitBtn.disabled = true
+        submissionError.style.display = 'none'
+        let ok = false;
+
+        (async () => {
+
+            submissionError.textContent = await fetch('http://localhost:3000/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    email: emailField.value,
+                    password: passwordField.value,
+                })
+            })
+                .then(res => {
+
+                    if (res.ok) {
+                        ok = true
+                        window.location.replace('http://localhost:3000/')
+                    }
+
+                    return res.text()
+                })
+
+            if (!ok) {
+                submissionError.style.display = 'block'
+                submitBtn.disabled = false
+            }
+
+        })()
+    }
+})
+
+function validateForm() {
+
+    let isValid = true
+
+    if (!requirementValidation(emailField)) {
+        addFieldError(emailField, 'This field is required.')
+        isValid = false
+    }
+
+    if (!requirementValidation(passwordField)) {
+        addFieldError(passwordField, 'This field is required.')
+        isValid = false
+    }
+
+    return true
+}
+
+function removeFieldError(field) {
+    field.classList.remove('error-field')
+    errorSpan = document.querySelector(`#${field.id} + .error-span`)
+    errorSpan.style.display = 'none'
+}
+
+function addFieldError(field, errorMessage) {
+    field.classList.add('error-field')
+    errorSpan = document.querySelector(`#${field.id} + .error-span`)
+    errorSpan.style.display = 'block'
+    errorSpan.textContent = errorMessage
+}
+
+function requirementValidation(field) {
+    return field.value !== ''
+}
+
+
